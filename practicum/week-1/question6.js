9ddcd9ff8997374c8ee8b9bc0301bb396635e15f
hamzahfirman <70231846+hamzahfirman@users.noreply.github.com>
2020-08-30 13:27:38 -0700

    Update question6.js

--- practicum/week-1/question6.js
+++ practicum/week-1/question6.js
@@ -6,5 +6,16 @@ output: 3
 */
 
 var median = function(input) {
+    var result= 0;
+    let numOfElements = input.length;
 
+    if(numOfElements % 2 === 0){
+        let num1 = input[(numOfElements / 2) -1];
+        let num2 = input[num1 +1];
+        result = (num1 + num2) / 2;
+    }else {
+        
+        result = input[Math.floor(numOfElements / 2)]
+    }
+    return result
 };
