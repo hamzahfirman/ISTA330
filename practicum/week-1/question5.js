4f5796f0d61e76675c9041ad3270f576cfe29093
hamzahfirman <70231846+hamzahfirman@users.noreply.github.com>
2020-08-30 11:52:17 -0700

    Update question5.js

--- practicum/week-1/question5.js
+++ practicum/week-1/question5.js
@@ -10,9 +10,23 @@ input: 12
 output: 3
 12 divided by 4 becomes 3 which divided by 3 becomes 1. The third and last step is the substraction by one operation.
 */
-
 var minimalReduction = function(n) {
-
+    let count = 0;
+    let current = n;
+    // Perform operations on 'n' 
+    while(current != 0){
+         // Operation #1
+        if((current / 4) % 1 === 0){
+            count+= 1;
+            current = current / 4;
+        // Operation #2
+        } else if((current / 3) % 1 === 0){
+            count+= 1;
+            current = current / 3;
+        }else{
+        // Operation #3
+        current -= 1;
+        count += 1;
+    } }
+    return count;
 };
-
-console.log('hi');
