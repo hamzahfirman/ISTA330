98475d2cf5cf81df64bd879445b3afe49441d0e8
hamzahfirman <70231846+hamzahfirman@users.noreply.github.com>
2020-09-12 21:42:40 -0700

    Update question10.js

--- practicum/week-1/question10.js
+++ practicum/week-1/question10.js
@@ -8,4 +8,18 @@ output: 7 - 2 = 5
 
 var maxMinusMin = function(n) {
 
+     var diff = 0;
+     var str = "" + n;
+     var min = str[0];
+     var max = str[0];
+
+     for(var i = 0; i < str; i++){
+         if(min > str[i]){
+             min = str[i];
+         };
+         if(max < str[i]){
+             max = str[i];
+         }
+     }
+     return max - min;
 };
