aa3cce3fdbaf2dab29016ac6b29ca880cf95d04e
hamzahfirman <hamzahfirman08@email.arizona.edu>
2020-09-23 16:50:19 -0700

    New

--- practicum/week-2/question1.js
+++ practicum/week-2/question1.js
@@ -10,27 +10,27 @@ is at least twice as much as every other number
 */
 
 var largestNumberIsAtleastTwice = function(input){
-        var maxNum = findMax(input);
-        var i;
+//         var maxNum = findMax(input);
+//         var i;
     
-        for(i= 0;i < input.length; i++){
-            if(input[i] != maxNum){
-                if(Math.floor(maxNum / input[i]) < 2){
-                    return false;
-                }
-            }
-        }
-        return true;
-    };
+//         for(i= 0;i < input.length; i++){
+//             if(input[i] != maxNum){
+//                 if(Math.floor(maxNum / input[i]) < 2){
+//                     return false;
+//                 }
+//             }
+//         }
+//         return true;
+//     };
     
-var findMax = function(arr){
-    var max = arr[0];   
-    var i;
+// var findMax = function(arr){
+//     var max = arr[0];   
+//     var i;
     
-    for(i = 0; i< arr.length; i++){
-        if(max < arr[i]){
-            max = arr[i];
-        }
-    }
-    return max;
+//     for(i = 0; i< arr.length; i++){
+//         if(max < arr[i]){
+//             max = arr[i];
+//         }
+//     }
+//     return max;
 };
