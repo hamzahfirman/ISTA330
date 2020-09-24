ba2ea2c64b0732685ce2ac40d232bb16c593c654
hamzahfirman <hamzahfirman08@email.arizona.edu>
2020-09-23 17:19:39 -0700

    Answered

--- practicum/week-2/question7.js
+++ practicum/week-2/question7.js
@@ -12,5 +12,22 @@ output: [[1,4], [2,5], [3,6]]
 */
 
 var transpose = function(M) {
+    var row = M.length;
+    var col = M[0].length;
+    var transposedM = [];
+    var index = 0;
 
+    var i;
+    var j;
+
+    for(i = 0; i < col; i++){
+        var temp = [];
+
+        for(j = 0; j < row; j++){
+            temp.push(M[j][index]);     
+        }
+        transposedM.push(temp);
+        index++;
+    }
+    return transposedM;
 };
