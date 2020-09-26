a682f48c8ecc0f74bf53674ab9e68e751cf087e6
hamzahfirman <hamzahfirman08@email.arizona.edu>
2020-09-26 12:28:46 -0700

    Answered

--- practicum/week-3/question1.js
+++ practicum/week-3/question1.js
@@ -20,5 +20,40 @@ Return the number of teams you can form given the conditions.
 */
 
 var howManyTeams = function(input) {
-   
+   var count= 0;
+   var i;
+   var j;
+
+   // Odd Length
+   if(input.length % 2 == 0){
+           var loopLength = input.length / 2;
+
+   }else{ // Even Length
+        var loopLength = Math.ceil(input.length / 2);
+
+   }
+
+   for(i = 0; i < loopLength; i++){
+           for(j = i + 1; j <= loopLength; j++){
+                var currArray = []
+                currArray.push(input[i]);
+                currArray.push(input[j]);
+                currArray.push(input[j+1]);
+
+                // VALIDATION PROCESS
+                if(isSortedOrNot(currArray, input.length) == true){
+                  count +=1;
+                }
+           }
+   }
+   return count;
 };
+
+var isSortedOrNot = function(arr, arrLength) {
+
+        if((arr[0] >= 0 )&& (arr[0] < arr[1]) && (arr[1] < arr[2]) && (arr[2] < arrLength)){
+                return true;
+        }
+        return false;
+}
+
