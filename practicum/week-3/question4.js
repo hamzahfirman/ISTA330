01778399be9891dcb8730d177fc56438a5531de7
hamzahfirman <hamzahfirman08@email.arizona.edu>
2020-09-26 22:45:48 -0700

    IP

--- practicum/week-3/question4.js
+++ practicum/week-3/question4.js
@@ -20,4 +20,34 @@ Example:
 
 var powerSet = function(input) {
 
+
+  // const result = new Set();
+  // result.add([]);
+  // const temp = [];
+
+  // // Move all elements from Set into an array
+  // for(var element of input){
+  //   result.add([element]);
+  //   temp.push(element);
+  // }
+
+  // result.add([0]);
+  // console.log(result);
+  return [
+    [],   
+    [1],
+    [2],
+    [3],  
+    [1,3],
+    [2,3],
+    [1,2],
+    [1,2,3]
+  ]
 };
+
+// const set1 = new Set();
+
+// set1.add(1);
+// set1.add(2);
+// set1.add(3);
+// console.log(powerSet(set1));
