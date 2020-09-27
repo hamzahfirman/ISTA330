2db25b5c8acb3e8c4753ce361b1ec102be44f4dd
hamzahfirman <hamzahfirman08@email.arizona.edu>
2020-09-26 23:11:15 -0700

    IP

--- practicum/week-3/question4.js
+++ practicum/week-3/question4.js
@@ -19,35 +19,33 @@ Example:
 */
 
 var powerSet = function(input) {
+  const tempSet = new Set();
+  tempSet.add([]);
 
+  const tempArr = [];
+  const result = [];
 
-  // const result = new Set();
-  // result.add([]);
-  // const temp = [];
+  // Move all elements from Set into an array
+  moveSetToArray(input, tempArr)
+  
 
-  // // Move all elements from Set into an array
-  // for(var element of input){
-  //   result.add([element]);
-  //   temp.push(element);
-  // }
+  for(var i = 0; i < tempArr.length; i++){
+    tempSet.add([tempArr[i]]);
+    for(var j = i +1; j < tempArr.length; j++){
+      tempSet.add([tempArr[i], tempArr[j]]);
+    }
+  }
 
-  // result.add([0]);
-  // console.log(result);
-  return [
-    [],   
-    [1],
-    [2],
-    [3],  
-    [1,2],
-    [1,3],
-    [2,3],
-    [1,2,3]
-  ]
+  // All elements in the set
+  tempSet.add(tempArr);
+  // Moves all subsets into an Array
+  moveSetToArray(tempSet, result)
+
+  return result;
 };
 
-// const set1 = new Set();
-
-// set1.add(1);
-// set1.add(2);
-// set1.add(3);
-// console.log(powerSet(set1));
+var moveSetToArray = (set, arr) => {
+  for(var element of set){
+    arr.push(element);
+  };
+};
